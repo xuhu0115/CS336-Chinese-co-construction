1169b467480a2fc880854d3545572c65040a0feb
kangkang <2043142525@qq.com>
2026-02-03 00:47:55 +0800

    修改检查

--- coursework/Assignment4_Data/cs336_data/demo.py
+++
@@ -1,5 +0,0 @@
-import fasttext
-
-# 禁用 mmap
-model = fasttext.load_model(r"/mnt/d/code/项目/cs336/CS336-Chinese-co-construction/coursework/Assignment4_Data/cs336_data/lid.176.bin", mmap=False)
-print(model.predict("This is a test."))
